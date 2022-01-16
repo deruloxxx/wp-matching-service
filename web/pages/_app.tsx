05b88fbb12df35cba47f7486cfdee2e8639c65a4
ryuyauetake <ryuya@anymindgroup.com>
2022-01-16 21:42:37 +0900

    Feat Footer

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {Footer} from "../components/layout/footer/Footer";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <Footer/>
+    </>
+  )
 }
 
 export default MyApp
