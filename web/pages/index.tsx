a745957332c2d42f29ecd922d4a75e34c37b32ec
ryuyauetake <ryuya@anymindgroup.com>
2022-01-16 22:53:52 +0900

    Feat intoducePrice

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,11 +1,13 @@
 import {Contact} from "../components/organisms/Form/Contact/Contact";
 import {IntroduceService} from "../components/organisms/IntroduceService/IntroduceService";
 import Image from "next/image";
+import {IntroducePrice} from "../components/organisms/IntroducePrice/IntroducePrice";
 
 const Index = () => {
   return (
     <>
       <IntroduceService/>
+      <IntroducePrice/>
       <Contact/>
     </>
 
