10f73ead02ee42e7e85a7b3450a1b26e994361f9
ryuyauetake <ryuya@anymindgroup.com>
2022-01-16 23:32:43 +0900

    Feat Hero Header

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,10 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import {Footer} from "../components/layout/footer/Footer";
+import {Footer} from "../components/layout/Footer/Footer";
+import {Header} from "../components/layout/Header/Header";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Header/>
       <main>
         <Component {...pageProps} />
       </main>
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -2,10 +2,12 @@ import {Contact} from "../components/organisms/Form/Contact/Contact";
 import {IntroduceService} from "../components/organisms/IntroduceService/IntroduceService";
 import Image from "next/image";
 import {IntroducePrice} from "../components/organisms/IntroducePrice/IntroducePrice";
+import {Hero} from "../components/organisms/Hero/Hero";
 
 const Index = () => {
   return (
     <>
+      <Hero/>
       <IntroduceService/>
       <IntroducePrice/>
       <Contact/>
