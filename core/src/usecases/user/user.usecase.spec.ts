692ad74b20f4efce76a9f9869b8b213cf3fcca42
ryuyauetake <ryuya@anymindgroup.com>
2022-01-23 16:32:05 +0900

    Feat User Register conditions about form

--- core/src/usecases/user/user.usecase.spec.ts
+++ core/src/usecases/user/user.usecase.spec.ts
@@ -7,6 +7,16 @@ describe('ユーザー登録', () => {
     expect(() => userUsecase.registerUser(new User())).not.toThrowError();
   });
 
+  test('訪問者が、新規ユーザー登録フォームの必須項目を "全て記入している" という条件の時できる', () => {
+    const userUsecase = new UserUsecase();
+    expect(() => userUsecase.registerUser(new User())).not.toThrowError();
+  });
+
+  test('訪問者が、新規ユーザー登録フォームの必須項目を "全て記入していない" という条件の時できない', () => {
+    const userUsecase = new UserUsecase();
+    expect(() => userUsecase.registerUser(new User())).toThrowError();
+  });
+
   test('訪問者が、新規ユーザー登録を "している" という条件の時できない', () => {
     const userUsecase = new UserUsecase();
     expect(() => userUsecase.registerUser(new User())).toThrowError();
