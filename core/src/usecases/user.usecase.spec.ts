aa5af87396575c079848b4f1e8dbb02407728a42
ryuyauetake <ryuya@anymindgroup.com>
2022-01-23 14:27:57 +0900

    Feat user register conditions

--- core/src/usecases/user.usecase.spec.ts
+++ core/src/usecases/user.usecase.spec.ts
@@ -2,17 +2,12 @@ import { UserUsecase } from './user.usecase';
 import { User } from '../models/user/user';
 
 describe('ユーザー登録', () => {
-  test('訪問者が、新規ユーザー登録を zzz という条件の時できる', () => {
+  test('訪問者が、新規ユーザー登録を "していない" という条件の時できる', () => {
     const userUsecase = new UserUsecase();
     expect(() => userUsecase.registerUser(new User())).not.toThrowError();
   });
 
-  test('訪問者が、新規ユーザー登録を xxx という条件の時できない', () => {
-    const userUsecase = new UserUsecase();
-    expect(() => userUsecase.registerUser(new User())).toThrowError();
-  });
-
-  test('訪問者が、新規ユーザー登録を yyy という条件の時できない', () => {
+  test('訪問者が、新規ユーザー登録を "している" という条件の時できない', () => {
     const userUsecase = new UserUsecase();
     expect(() => userUsecase.registerUser(new User())).toThrowError();
   });
